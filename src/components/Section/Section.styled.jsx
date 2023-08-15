b808267869ca14f53bdf7b38347da23e16f749f3
Demian <demyan.korotchenko@gmail.com>
2023-08-15 23:30:36 +0200

    111

--- src/components/Section/Section.styled.jsx
+++ src/components/Section/Section.styled.jsx
@@ -4,8 +4,7 @@ export const SectionWrapper = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: ${props => props.theme.space[4]}px;
-  padding-bottom: ${props => props.theme.space[4]}px;
+  
 `;
 
 export const Title = styled.h1``;
